2b1a6f5ab20e5b3714c78ccd0a3c41edc2aa76c9
McKnight <babaevtamerlan3@gmail.com>
2026-01-02 12:51:03 +0400

    Third Commit

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -3,17 +3,17 @@ import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import { PanelLeft } from "lucide-react";
 
-import { useIsMobile } from "../hooks/use-mobile";
+import { useIsMobile } from "../../hooks/use-mobile";
 import { cn } from "../lib/utils";
-import { Button } from "../components/ui/Button";
-import { Input } from "../components/ui/input";
-import { Separator } from "../components/ui/separator";
-import { Sheet, SheetContent } from "../components/ui/sheet";
-import { Skeleton } from "../components/ui/skeleton";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../components/ui/tooltip";
+import { Button } from "../../components/ui/Button";
+import { Input } from "../../components/ui/input";
+import { Separator } from "../../components/ui/separator";
+import { Sheet, SheetContent } from "../../components/ui/sheet";
+import { Skeleton } from "../../components/ui/skeleton";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../../components/ui/tooltip";
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state";
-const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;+
+const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 const SIDEBAR_WIDTH = "16rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
 const SIDEBAR_WIDTH_ICON = "3rem";
