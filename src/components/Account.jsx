53107dde4d39a5a65afafa1bd4381ace073627da
McKnight <babaevtamerlan3@gmail.com>
2026-01-04 09:09:24 +0400

    Alpha

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { CircleUserRound, Mail, Lock } from 'lucide-react';
 
 export const Account = () => {
-  return (
-    <h1>Moshi Moshi</h1>
-  )
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">
+                    <CircleUserRound />
+                    Qeydiyyatdan Keçim
+                </div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <Mail />
+                    <input type="email" />
+                </div>
+                <div className="input">
+                    <Lock />
+                    <input type="password" />
+                </div>
+            </div>
+            <div className="forgot-password">Şifrəni unutdum <span /></div>
+            <div className="submit-container">
+                <div className="submit">Qeydiyyatdan Keçim</div>
+                <div className="submit">Daxil Olun</div>
+            </div>
+        </div>
+    )
 }
